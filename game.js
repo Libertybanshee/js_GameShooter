29f7bdb3a354b2571de6956a694c9e81033736c1
Libertybanshee <Libertybanshee1@gmail.com>
2022-09-03 12:13:46 +0200

    cours GC

--- game.js
+++ game.js
@@ -1,17 +1,83 @@
 let img;
 let imgG;
-let x = 0;
-let y = 0;
+let timer;
 
-let derniereUpdate = Date.now()
+let KeyUp = false;
+let KeyRight = false;
+let KeyDown = false;
+let KeyLeft = false;
 
-function load() {
-    img = new Sprite("img/Hero/Hero1.png", 100, 100);
-    imgG = new Sprite("img/ennemiGreen/green1.png", 300, 300);
+function toucheEnfoncee(t) {
+    t.preventDefault();
+    if (t.code == "KeyW") {
+        KeyUp = true;
+    }
+
+    if (t.code == "KeyD") {
+        KeyRight = true;
+    }
+    
+    if (t.code == "KeyS") {
+        KeyDown = true;
+    }
+    
+    if (t.code == "KeyA") {
+        KeyLeft = true;
+    }
 }
 
-function update() {
-    img.x;
+function toucheRelachee(t) {
+    t.preventDefault();
+    if (t.code == "KeyW") {
+        KeyUp = false;
+    }
+
+    if (t.code == "KeyD") {
+        KeyRight = false;
+    }
+    
+    if (t.code == "KeyS") {
+        KeyDown = false;
+    }
+    
+    if (t.code == "KeyA") {
+        KeyLeft = false;
+    }
+    
+}
+
+function load() {
+    document.addEventListener("keydown", toucheEnfoncee, false);
+    document.addEventListener("keyup", toucheRelachee, false);
+
+    img = new Sprite("img/Hero/Hero1.png", 100, 100);
+    imgG = new Sprite("img/ennemiGreen/green1.png", 300, 300);
+
+    timer = 0;
+}
+
+function update(dt) {
+    timer += dt;
+    if (timer >= 1) {
+        imgG.x++;
+        timer = 0;
+    }
+
+    if (KeyUp) {
+        img.y -= 10;
+    }
+
+    if (KeyRight) {
+        img.x += 10;
+    }
+
+    if (KeyDown) {
+        img.y += 10;
+    }
+
+    if (KeyLeft) {
+        img.x -= 10;
+    }
 }
 
 function draw(pCtx) {
--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-let canvas = document.getElementById("canvas")
-let ctx = canvas.getContext("2d")
-let interval
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let interval;
+let fps = 0;
 
-function run() {
+let derniereUpdate = 0;
+
+function showFPS() {
+    ctx.fillStyle = "White";
+    ctx.font = "normal 16pt Arial";
+
+    ctx.fillText(Math.floor(fps) + " fps", 10, 20)
+}
+
+function run(time) {
+    requestAnimationFrame(run);
+    let dt = (time - derniereUpdate) / 1000;
+
+    // limiter les FPS
+    if (dt < (1 / 60) - 0.001) {
+        return;
+    }
+
+    fps = 1 / dt
+    derniereUpdate = time;
     console.log("Run");
-    update();
+    update(dt);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw(ctx);
+    showFPS();
 }
 
 function init() {
     console.log("Init");
     load()
-    interval = setInterval(run, 1000 / 60);
+    requestAnimationFrame(run);
 }
 
 init()
